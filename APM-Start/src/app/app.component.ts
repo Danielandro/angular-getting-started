37ccc4d89345675a0f041c126964a7dea06a3a07
Danielandro <mtme13@hotmail.com>
2020-01-13 12:50:59 +0000

    Intial commit

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
+  selector: 'pm-root', // directive name
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
