c0af2e5d0a68796edaa28d84c9d81977effa718c
Danny <mtme13@hotmail.com>
2020-01-20 15:19:37 +0000

    Passing ID parameter to product detail route

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { IProduct } from "./product";
+import { runInThisContext } from "vm";
 
 @Component({
-  selector: 'pm-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  selector: "pm-product-detail",
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"]
 })
 export class ProductDetailComponent implements OnInit {
+  product: IProduct; // product details being displayed
+  pageTitle: string = "Product Detail";
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // + converts string to number
+    let id = +this.route.snapshot.paramMap.get("id");
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: "Hammer",
+      productCode: "TBX-0048",
+      releaseDate: "May 21, 2019",
+      description: "Curved claw steel hammer",
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl: "assets/images/hammer.png"
+    };
   }
-
 }
