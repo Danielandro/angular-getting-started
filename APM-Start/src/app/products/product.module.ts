d3e29c0bbca6760af9e03efbcba54361fbb44d6a
Danny <mtme13@hotmail.com>
2020-01-22 16:45:24 +0000

    Reorganise project into modules * create Product feature module * create Shared feature module

---
+++ APM-Start/src/app/products/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProductListComponent } from "./product-list.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { ProductDetailGuard } from "./product-detail.guard";
+import { ConvertToSpaces } from "../shared/convert-to-spaces.pipe";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+  declarations: [ProductListComponent, ProductDetailComponent, ConvertToSpaces],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: "products", component: ProductListComponent },
+      {
+        path: "products/:id",
+        component: ProductDetailComponent,
+        canActivate: [ProductDetailGuard]
+      }
+    ]),
+    SharedModule
+  ]
+})
+export class ProductModule {}
