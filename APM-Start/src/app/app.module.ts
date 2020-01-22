ad313247dd9893fa88fe3e8af66b98afcf2705e6
Danny <mtme13@hotmail.com>
2020-01-22 17:17:57 +0000

    Practise using modules * reorganise file structure * add module for App routing

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,22 +3,13 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { RouterModule } from "@angular/router";
 import { WelcomeComponent } from "./home/welcome.component";
 import { ProductModule } from "./products/product.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [AppComponent, WelcomeComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: "welcome", component: WelcomeComponent },
-      { path: "", redirectTo: "/welcome", pathMatch: "full" },
-      { path: "**", component: WelcomeComponent }
-    ]),
-    ProductModule
-  ],
+  imports: [BrowserModule, HttpClientModule, ProductModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ProductListComponent } from "./product-list.component";
-import { ProductDetailComponent } from "./product-detail.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ProductDetailGuard } from "./product-detail.guard";
+import { ProductDetailGuard } from "./product-detail/product-detail.guard";
 import { ConvertToSpaces } from "../shared/convert-to-spaces.pipe";
 import { SharedModule } from "../shared/shared.module";
 
