bece15b61b42b5fbbcb4b42e7ac5cb2eafa7cb14
Danny <mtme13@hotmail.com>
2020-01-22 17:33:49 +0000

    Add module for product routing

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { WelcomeComponent } from "./home/welcome.component";
 import { ProductModule } from "./products/product.module";
+
 import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -3,25 +3,17 @@ import { CommonModule } from "@angular/common";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-import { ProductDetailGuard } from "./product-detail/product-detail.guard";
 import { ConvertToSpaces } from "../shared/convert-to-spaces.pipe";
 import { SharedModule } from "../shared/shared.module";
+import { ProductRoutingModule } from "./product-routing.module";
 
 @NgModule({
-  declarations: [ProductListComponent, ProductDetailComponent, ConvertToSpaces],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([
-      { path: "products", component: ProductListComponent },
-      {
-        path: "products/:id",
-        component: ProductDetailComponent,
-        canActivate: [ProductDetailGuard]
-      }
-    ]),
-    SharedModule
-  ]
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpaces
+  ],
+  imports: [CommonModule, FormsModule, SharedModule,
+    ProductRoutingModule]
 })
 export class ProductModule {}
